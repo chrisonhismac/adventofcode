8743df7ef9a020fd173f23853f0f25ca6b5aa7b7
Chris McGuire <chris.mcguire@thinkific.com>
2022-12-05 21:42:35 -0800

    end of day5

--- day5/day5.php
+++ day5/day5.php
@@ -1,35 +1,90 @@
 <?php
 
-$stack1 = array();
-$stack2 = array();
-$stack3 = array();
-$stack4 = array();
-$stack5 = array();
-$stack6 = array();
-$stack7 = array();
-$stack8 = array();
+$stack1 = array("F","T","N","Z","M","G","H","J");
+$stack2 = array("J","W","V");
+$stack3 = array("H","T","B","J","L","V","G");
+$stack4 = array("L","V","D","C","N","J","P","B");
+$stack5 = array("G","R","P","M","S","W","F");
+$stack6 = array("M","V","N","B","F","C","H","G");
+$stack7 = array("R","M","G","H","D");
+$stack8 = array("D","Z","V","M","N","H");
+$stack9 = array("H","F","N","G");
 
 $input = file('input.txt');
 $output1 = 0;
 $output2 = 0;
 
+//Part 1
 foreach($input as $moves) {
+    
+    //parse the line
     $parsechange = str_replace(array("move ", " from ", " to "), array("", "-", "-"), $moves);
     $movesarray = explode("-",$parsechange);
-    $from = ${"stack".$movesarray[1]};
-    $to = ${"stack".$movesarray[2]};
+    $movesarray = array_map('intval', $movesarray);
+
+    //set variable
     $numberofboxes = $movesarray[0]; 
+    $fromstack = ${"stack".$movesarray[1]};
+    $tostack = ${"stack".$movesarray[2]};
+    
+    $fromcount = count($fromstack);
     $x = 1;
+
+    if ($numberofboxes > count($fromstack)){
+        var_dump(${"stack".$movesarray[1]});
+        var_dump($stack6);
+        die;
+    }
+
+    //loop over number of boxes to be moved
     while($x <= $numberofboxes) {
-        echo "The number is: $x\n";
+        array_unshift($tostack,$fromstack[0]);
+        unset($fromstack[0]);
+        $fromstack = array_values($fromstack);
+        $tostack = array_values($tostack);
+        ${"stack".$movesarray[1]} = $fromstack;
+        ${"stack".$movesarray[2]} = $tostack;
         $x++;
     }
-    //$movesarray[0] = how many boxes to move
-
-    //array_unshift($stack, what to add);
-    //unset($stacck, key to remove);
 }
 
+//echo the top of each stack 
+echo "Part 1: " . $stack1[0] .$stack2[0] . $stack3[0] . $stack4[0] . $stack5[0] . $stack6[0] . $stack7[0] . $stack8[0] . $stack9[0] . "\n";
 
-echo "Part 1: " . $output1 . "\n";
-echo "Part 2: " . $output2 .  "\n";
+//Part 2
+$stack1 = array("F","T","N","Z","M","G","H","J");
+$stack2 = array("J","W","V");
+$stack3 = array("H","T","B","J","L","V","G");
+$stack4 = array("L","V","D","C","N","J","P","B");
+$stack5 = array("G","R","P","M","S","W","F");
+$stack6 = array("M","V","N","B","F","C","H","G");
+$stack7 = array("R","M","G","H","D");
+$stack8 = array("D","Z","V","M","N","H");
+$stack9 = array("H","F","N","G");
+foreach($input as $moves) {
+    
+    //parse the line
+    $parsechange = str_replace(array("move ", " from ", " to "), array("", "-", "-"), $moves);
+    $movesarray = explode("-",$parsechange);
+    $movesarray = array_map('intval', $movesarray);
+
+    //set variable
+    $numberofboxes = $movesarray[0]; 
+    $fromstack = ${"stack".$movesarray[1]};
+    $tostack = ${"stack".$movesarray[2]};
+    
+
+    $toaddtofront = array_slice($fromstack, 0, $numberofboxes);
+    $tostack = array_merge($toaddtofront,$tostack);
+    $fromstack = array_slice($fromstack, $numberofboxes);
+    //re-index arrays
+    $fromstack = array_values($fromstack);
+    $tostack = array_values($tostack);
+    //assign to global variables
+    ${"stack".$movesarray[1]} = $fromstack;
+    ${"stack".$movesarray[2]} = $tostack;
+
+}
+
+//echo the top of each stack 
+echo "Part 2: " . $stack1[0] .$stack2[0] . $stack3[0] . $stack4[0] . $stack5[0] . $stack6[0] . $stack7[0] . $stack8[0] . $stack9[0] . "\n";
